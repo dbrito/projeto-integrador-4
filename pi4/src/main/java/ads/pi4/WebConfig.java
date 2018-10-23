799829d23bf4f9f272adc81b7c5e0f13d246e04a
douglas <doug.dbrito@gmail.com>
2018-10-22 22:49:10 -0200

    Modal/Formulario de produto + Upload de arquivo

---
+++ pi4/src/main/java/ads/pi4/WebConfig.java
@@ -0,0 +1,22 @@
+package ads.pi4;
+
+import java.util.Locale;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+      registry.addResourceHandler("/estaticos/**")
+              .addResourceLocations("C:/Jobs/uploads");
+    }
+}
--- pi4/src/main/java/ads/pi4/controller/admin/AdminController.java
+++ pi4/src/main/java/ads/pi4/controller/admin/AdminController.java
@@ -6,6 +6,9 @@ import ads.pi4.model.Produto;
 import ads.pi4.model.Usuario;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.logging.Level;
@@ -23,13 +26,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
-    @GetMapping({"/"})    
+    @GetMapping({"/"}) //Tela de Login    
     public String login(HttpServletRequest req) {
         System.out.println("/");
         HttpSession sessao = req.getSession(true);            
@@ -39,7 +43,7 @@ public class AdminController {
         return "admin/login";
     }
     
-    @PostMapping("/")
+    @PostMapping("/") //Efetuar Login
     public ResponseEntity<Object> login(@RequestParam(value = "user", required = true) String user, @RequestParam(value = "pass", required = true) String pass, HttpServletRequest req) {                
         try {
             HttpSession sessao = req.getSession(true);            
@@ -56,14 +60,14 @@ public class AdminController {
         }                
     }
     
-    @GetMapping("/logout")    
+    @GetMapping("/logout") //Efetuar Logout
     public String logout(HttpServletRequest req) {        
         HttpSession sessao = req.getSession(true);            
         sessao.removeAttribute("admin");
         return "redirect:/admin/";
     }
     
-    @GetMapping("/painel")
+    @GetMapping("/painel") //Tela de Painel Administrativo
     public String painel(HttpServletRequest req) {
         HttpSession sessao = req.getSession(true);            
         if (sessao.getAttribute("admin") == null) {
@@ -71,6 +75,22 @@ public class AdminController {
         }
         
         return "admin/painel";
+    }
+    
+    @PostMapping("/api/upload")
+    public ResponseEntity<Object> login(@RequestParam("arquivo") MultipartFile arquivo, HttpServletRequest req) {                
+        try {            
+//            if (req.getSession(true).getAttribute("admin") != null) {;;
+                byte[] bytesArquivo = arquivo.getBytes();
+                Path destino = Paths.get("C:/Jobs/uploads/" + arquivo.getOriginalFilename());
+                Files.write(destino, bytesArquivo);                
+                return ResponseEntity.status(HttpStatus.OK).body("Envio de arquivo efetuado com sucesso.");                
+//            } else {
+//                return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Usuario ou senha inválidos.");                
+//            }            
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro ao realizar o upload do arquivo");                
+        }                
     }
     
 //    public ResponseEntity<Object> cadastar(@RequestBody Produto pd) {
@@ -84,7 +104,7 @@ public class AdminController {
 //    };
         
     
-    @GetMapping("/api/produtos")
+    @GetMapping("/api/produtos") //JSON com a lista de Produtos
     public void listarProdutos(HttpServletResponse response) {
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");                                                                 
@@ -97,7 +117,7 @@ public class AdminController {
         }
     }
     
-    @DeleteMapping("/api/produtos/{id}")
+    @DeleteMapping("/api/produtos/{id}") //Excluir um produto especifico
     public ResponseEntity<Object> excluirProduto(@PathVariable("id") int id, HttpServletResponse response) {
         try {
             ProdutoDAO.excluir(id);
