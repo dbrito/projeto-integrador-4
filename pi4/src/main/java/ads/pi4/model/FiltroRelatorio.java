e1ae797d016ae9b2d67c605d62eaa36b7141c2dd
Vihgalvao <victorgabrielsg@gmail.com>
2018-12-13 12:03:53 -0200

    Inclusão Model Filtro Relatório.

---
+++ pi4/src/main/java/ads/pi4/model/FiltroRelatorio.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ads.pi4.model;
+
+/**
+ *
+ * @author victor
+ */
+public class FiltroRelatorio {
+    
+    private String dtini;
+    private String dtfim;
+
+    public FiltroRelatorio() {
+    }
+
+    public String getDtini() {
+        return dtini;
+    }
+
+    public void setDtini(String dtini) {
+        this.dtini = dtini;
+    }
+
+    public String getDtfim() {
+        return dtfim;
+    }
+
+    public void setDtfim(String dtfim) {
+        this.dtfim = dtfim;
+    }
+    
+    
+}
